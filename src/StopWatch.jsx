f591dbbf4fa1f7ec2459a92c1b5642a27fd44845
shubham <thoratshubham161@gmail.com>
2024-02-08 12:19:59 +0530

    Stopwatch component added

---
+++ src/StopWatch.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const StopWatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  const timerID = useRef();
+
+  const formatTime = (time) => {
+    const min = Math.floor(time / 60);
+    const sec = time % 60;
+    return `${min}:${sec < 10 ? "0" + sec : sec}`;
+  };
+
+  useEffect(() => {
+    timerID.current = setTimeout(()=>{
+        if(isRunning)
+            setTime((prv)=> prv + 1);
+    }, 1000)
+
+    return () => {
+        clearTimeout(timerID.current);
+    }
+
+  }, [isRunning, time])
+
+  const handleToggel = () => setIsRunning(!isRunning);
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <p>Time: {formatTime(time)}</p>
+      <button onClick={handleToggel}>{isRunning ? "Stope" : "Start"}</button>
+      <button>Reset</button>
+    </div>
+  );
+};
+
+export default StopWatch;
